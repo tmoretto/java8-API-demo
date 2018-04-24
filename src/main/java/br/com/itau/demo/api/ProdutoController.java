0ead6bb5bcdbc1147eb63437fa7ac6f64acacb99
Tiago Moretto <moretto.tiago@gmail.com>
2018-04-24 20:52:45 -0300

    Incluindo Controller de Produto

---
+++ src/main/java/br/com/itau/demo/api/ProdutoController.java
@@ -0,0 +1,31 @@
+package br.com.itau.demo.api;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.itau.demo.model.Produto;
+import br.com.itau.demo.repository.ProdutoRepository;
+
+@RestController
+@RequestMapping("/api/produtos")
+public class ProdutoController {
+
+	@Autowired
+	private ProdutoRepository service;
+
+	@GetMapping
+	public ResponseEntity<List<Produto>> obterTodos() {
+		List<Produto> produtos = service.findAll();
+		if (produtos.isEmpty()) {
+			return new ResponseEntity<List<Produto>>(HttpStatus.NO_CONTENT);
+		}
+		return new ResponseEntity<List<Produto>>(produtos, HttpStatus.OK);
+	}
+	
+}
