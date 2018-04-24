798924857f06b831b88d34f49402e9885b87eab8
Tiago Moretto <moretto.tiago@gmail.com>
2018-04-24 20:31:30 -0300

    Incluindo modelo do Produto

---
+++ src/main/java/br/com/itau/demo/model/Produto.java
@@ -0,0 +1,80 @@
+package br.com.itau.demo.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Produto implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+ 	private String codigoBarras;
+
+    private String nome;
+    
+    public Produto() {
+		super();
+    }
+    
+	public Produto(String codigoBarras, String nome) {
+		super();
+		this.codigoBarras = codigoBarras;
+		this.nome = nome;
+	}
+
+	public String getCodigoBarras() {
+		return codigoBarras;
+	}
+
+	public void setCodigoBarras(String codigoBarras) {
+		this.codigoBarras = codigoBarras;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((codigoBarras == null) ? 0 : codigoBarras.hashCode());
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Produto other = (Produto) obj;
+		if (codigoBarras == null) {
+			if (other.codigoBarras != null)
+				return false;
+		} else if (!codigoBarras.equals(other.codigoBarras))
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Produto [codigoBarras=" + codigoBarras + ", nome=" + nome + "]";
+	}
+
+}
