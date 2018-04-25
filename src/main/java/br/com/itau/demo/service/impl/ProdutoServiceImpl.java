04debfa3c777964b11213175e432c19dabe4d56d
Tiago Moretto <moretto.tiago@gmail.com>
2018-04-24 21:09:56 -0300

    Incluindo anotacao de serviço

--- src/main/java/br/com/itau/demo/service/impl/ProdutoServiceImpl.java
+++ src/main/java/br/com/itau/demo/service/impl/ProdutoServiceImpl.java
@@ -3,11 +3,13 @@ package br.com.itau.demo.service.impl;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import br.com.itau.demo.model.Produto;
 import br.com.itau.demo.repository.ProdutoRepository;
 import br.com.itau.demo.service.ProdutoService;
 
+@Service
 public class ProdutoServiceImpl implements ProdutoService {
 
 	@Autowired
