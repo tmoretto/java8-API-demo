d047a97a781caad3c939dd416b395b9242925ad6
Tiago Moretto <moretto.tiago@gmail.com>
2018-04-24 21:20:38 -0300

    Incluindo classe utilitária para mapeamento dos objetos para JSON

---
+++ src/main/java/utils/ObjectMapperUtils.java
@@ -0,0 +1,32 @@
+package utils;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.modelmapper.ModelMapper;
+
+public class ObjectMapperUtils {
+	
+	private static ModelMapper modelMapper = new ModelMapper();
+
+	static {
+		modelMapper = new ModelMapper();
+	}
+
+	private ObjectMapperUtils() {
+	}
+
+	public static <D, T> D map(final T entity, Class<D> outClass) {
+		return modelMapper.map(entity, outClass);
+	}
+
+	public static <S, D> D map(final S source, D destination) {
+		modelMapper.map(source, destination);
+		return destination;
+	}
+	
+	public static <D, T> List<D> mapAll(final Collection<T> entityList, Class<D> outCLass) {
+		return entityList.stream().map(entity -> map(entity, outCLass)).collect(Collectors.toList());
+	}
+}
