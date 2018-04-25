64f4b28f0a8d0ac1988240f58a7f97bb0f803e71
Tiago Moretto <moretto.tiago@gmail.com>
2018-04-24 21:02:41 -0300

    Iniciando teste do ProdutoService

---
+++ src/test/java/br/com/itau/demo/service/ProdutoServiceTest.java
@@ -0,0 +1,14 @@
+package br.com.itau.demo.service;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import br.com.itau.demo.config.ConfigurationTest;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = ConfigurationTest.class)
+public class ProdutoServiceTest {
+
+
+}
