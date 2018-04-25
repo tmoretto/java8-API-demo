a46181fc599ff86c9258d2a67eb7f543d56c09e0
Tiago Moretto <moretto.tiago@gmail.com>
2018-04-24 21:22:40 -0300

    Refatorando para utilizar DTO no retorno do serviço

--- src/main/java/br/com/itau/demo/api/ProdutoController.java
+++ src/main/java/br/com/itau/demo/api/ProdutoController.java
@@ -9,8 +9,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.itau.demo.model.Produto;
+import br.com.itau.demo.dto.ProdutoDTO;
 import br.com.itau.demo.service.ProdutoService;
+import utils.ObjectMapperUtils;
 
 @RestController
 @RequestMapping("/api/produtos")
@@ -20,12 +21,12 @@ public class ProdutoController {
 	private ProdutoService service;
 
 	@GetMapping
-	public ResponseEntity<List<Produto>> obterTodos() {
-		List<Produto> produtos = service.obterTodos();
-		if (produtos.isEmpty()) {
-			return new ResponseEntity<List<Produto>>(HttpStatus.NO_CONTENT);
+	public ResponseEntity<List<ProdutoDTO>> obterTodos() {
+		List<ProdutoDTO> produtosDTO = ObjectMapperUtils.mapAll(service.obterTodos(), ProdutoDTO.class);
+		if (produtosDTO.isEmpty()) {
+			return new ResponseEntity<List<ProdutoDTO>>(HttpStatus.NO_CONTENT);
 		}
-		return new ResponseEntity<List<Produto>>(produtos, HttpStatus.OK);
+		return new ResponseEntity<List<ProdutoDTO>>(produtosDTO, HttpStatus.OK);
 	}
 	
 }
---
+++ src/main/java/br/com/itau/demo/dto/ProdutoDTO.java
@@ -0,0 +1,25 @@
+package br.com.itau.demo.dto;
+
+public class ProdutoDTO {
+
+	String codigoBarras;
+	
+	String nome;
+
+	public String getCodigoBarras() {
+		return codigoBarras;
+	}
+
+	public void setCodigoBarras(String codigoBarras) {
+		this.codigoBarras = codigoBarras;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+}
