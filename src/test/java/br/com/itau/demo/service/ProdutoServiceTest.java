a42e85f5cc7e18c36a65ed2be3717a09f6ce7e35
Tiago Moretto <moretto.tiago@gmail.com>
2018-04-24 21:41:45 -0300

    Criando teste para inclusao de Produto

--- src/test/java/br/com/itau/demo/service/ProdutoServiceTest.java
+++ src/test/java/br/com/itau/demo/service/ProdutoServiceTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
 
+import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,17 +18,30 @@ import br.com.itau.demo.model.Produto;
 @RunWith(SpringRunner.class)
 @ContextConfiguration(classes = ConfigurationTest.class)
 public class ProdutoServiceTest {
-	
+
 	@Autowired
 	private ProdutoService service;
 	
 	@Test
-	@Sql({"classpath:insert_produtos.sql"})
+	@Sql({ "classpath:insert_produtos.sql" })
+	@Sql(scripts = "classpath:limpa_dados.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
 	public void deveRetornarTodosProdutosTest() {
 		List<Produto> todosProdutos = service.obterTodos();
 		assertThat(todosProdutos).isNotNull();
 		assertThat(todosProdutos.isEmpty()).isEqualTo(Boolean.FALSE);
 		assertThat(todosProdutos.size()).isEqualTo(2);
+	}
+
+	@Test
+	@Sql(scripts = "classpath:limpa_dados.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+	public void deveIncluirUmNovoProdutoTest() {
+		Produto novoProduto = service.incluir(new Produto("7891048050262", "GELATINA DR.OETKER MORANGO"));
+		assertThat(novoProduto).isNotNull();
+
+		List<Produto> todosProdutos = service.obterTodos();
+		assertThat(todosProdutos).isNotNull();
+		assertThat(todosProdutos.isEmpty()).isEqualTo(Boolean.FALSE);
+		assertThat(todosProdutos.size()).isEqualTo(1);
 	}
 
 }
