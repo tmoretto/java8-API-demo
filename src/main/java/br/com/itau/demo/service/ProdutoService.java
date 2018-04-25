5483e0011813334b2bd78730439a8fec54d51d71
Tiago Moretto <moretto.tiago@gmail.com>
2018-04-24 21:06:22 -0300

    Criando teste para retornar todos produtos via Service

---
+++ src/main/java/br/com/itau/demo/service/ProdutoService.java
@@ -0,0 +1,11 @@
+package br.com.itau.demo.service;
+
+import java.util.List;
+
+import br.com.itau.demo.model.Produto;
+
+public interface ProdutoService {
+
+	List<Produto> obterTodos();
+
+}
--- src/test/java/br/com/itau/demo/service/ProdutoServiceTest.java
+++ src/test/java/br/com/itau/demo/service/ProdutoServiceTest.java
@@ -1,14 +1,33 @@
 package br.com.itau.demo.service;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import br.com.itau.demo.config.ConfigurationTest;
+import br.com.itau.demo.model.Produto;
 
 @RunWith(SpringRunner.class)
 @ContextConfiguration(classes = ConfigurationTest.class)
 public class ProdutoServiceTest {
-
+	
+	@Autowired
+	private ProdutoService service;
+	
+	@Test
+	@Sql({"classpath:insert_produtos.sql"})
+	public void deveRetornarTodosProdutosTest() {
+		List<Produto> todosProdutos = service.obterTodos();
+		assertThat(todosProdutos).isNotNull();
+		assertThat(todosProdutos.isEmpty()).isEqualTo(Boolean.FALSE);
+		assertThat(todosProdutos.size()).isEqualTo(2);
+	}
 
 }
