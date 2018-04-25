4316ce1e71d69c4c20ba74a40bfd229465727af8
Tiago Moretto <moretto.tiago@gmail.com>
2018-04-24 21:57:52 -0300

    Implementando POST para inclusão de Produto

--- src/main/java/br/com/itau/demo/api/ProdutoController.java
+++ src/main/java/br/com/itau/demo/api/ProdutoController.java
@@ -1,16 +1,24 @@
 package br.com.itau.demo.api;
 
+import java.net.URI;
 import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import br.com.itau.demo.dto.ProdutoDTO;
 import br.com.itau.demo.model.Produto;
 import br.com.itau.demo.service.ProdutoService;
+import utils.ObjectMapperUtils;
 
 @RestController
 @RequestMapping("/api/produtos")
@@ -20,12 +28,23 @@ public class ProdutoController {
 	private ProdutoService service;
 
 	@GetMapping
-	public ResponseEntity<List<Produto>> obterTodos() {
-		List<Produto> produtos = service.obterTodos();
-		if (produtos.isEmpty()) {
-			return new ResponseEntity<List<Produto>>(HttpStatus.NO_CONTENT);
+	public ResponseEntity<List<ProdutoDTO>> obterTodos() {
+		List<ProdutoDTO> produtosDTO = ObjectMapperUtils.mapAll(service.obterTodos(), ProdutoDTO.class);
+		if (produtosDTO.isEmpty()) {
+			return new ResponseEntity<List<ProdutoDTO>>(HttpStatus.NO_CONTENT);
 		}
-		return new ResponseEntity<List<Produto>>(produtos, HttpStatus.OK);
+		return new ResponseEntity<List<ProdutoDTO>>(produtosDTO, HttpStatus.OK);
+	}
+	
+	@PostMapping
+	public ResponseEntity<ProdutoDTO> incluir(@RequestBody Produto novoProduto, HttpServletResponse response) {
+		Produto produto = service.incluir(novoProduto);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigoBarras}")
+				.buildAndExpand(produto.getCodigoBarras()).toUri();
+		response.setHeader("Location", uri.toASCIIString());
+		
+		return ResponseEntity.created(uri).body(ObjectMapperUtils.map(produto, ProdutoDTO.class));
 	}
 	
 }
